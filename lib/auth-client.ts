23343f9e5e27fc7d9e46be8fb37085a28e8892b6
shishir <mdabdulaziz3745@gmail.com>
2026-03-08 02:19:50 +0600

    .

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: typeof window !== "undefined" ? window.location.origin : "",
+  // baseURL: typeof window !== "undefined" ? window.location.origin : "",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL || "https://pharma-care-1.onrender.com",
   fetchOptions: {
     credentials: "include",
   },
